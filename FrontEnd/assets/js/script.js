b7b3eea05932c1fae945298e7247f00a49856d53
Raphaelle Goulong <raphaelle.goulong@gmail.com>
2023-10-18 16:11:25 +0200

    feat : button Filter

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -4,9 +4,10 @@ fetch("http://localhost:5678/api/works")
   // On transforme les data en Json
   .then((res) => res.json())
   // On peut utiliser les data
-  .then((data) => {
+  .then((data, dataFilter) => {
     // regarder ce qu'on reçoit pour bien cibler l'objet
     buildHtml(data)
+    photos = document.querySelectorAll(".gallery img");
     // Ici je veux qu'un seul user, donc j'envoie [0] pour le 1er user 
   })
  
@@ -26,6 +27,7 @@ fetch("http://localhost:5678/api/works")
     for (let i = 0; i < data.length; i++) {
         // Créez une figure
         let figure = document.createElement("figure");
+        figure.classList.add("figureContainer")
         figure.setAttribute("data-cat",data[i].categoryId)
         myProjets.appendChild(figure);
 
@@ -40,16 +42,59 @@ fetch("http://localhost:5678/api/works")
         imagesTexts.innerHTML = data[i].title;
         figure.appendChild(imagesTexts);
     }
-}
-    
-
+  }
+  
+  
+  function buildFilter(dataFilter) {
+    let buttonContainer = document.querySelector(".btn-box");
+  //  cette boucle permet de crée des boutons avec les bons noms de catégorie
+    for (let i = 0; i < dataFilter.length; i++) {
+      let button = document.createElement("button");
+      button.classList.add("btn-sorting");
+      button.textContent = dataFilter[i].name;
+  // cet évènement permet de faire apparaitre et disparaitre les images ainsi que les figcaptions
+      button.addEventListener("click", () => {
+        let categoryId = dataFilter[i].id;
+        console.log(categoryId);
+        let figures = document.getElementsByClassName("figureContainer")
+        const figureArray = Array.from(figures);
+        console.log(figures);
+        
+        for (let j = 0; j < figures.length; j++) {
+        //   // cette ligne cherche le parent ou le grand parent LE PLUS PROCHE de l'image de la boucle
+        //   let figure = photos[j].closest("figure"); 
+        //   let dataCat = figure.getAttribute("data-cat");
+          
+          if (categoryId === figures[j].getAttribute("data-cat") ) {
+            figures[j].style.display = "block";
+            // let figcaption = figure.querySelector("figcaption");
+            // if (figcaption) {
+            //   figcaption.style.display = "none";
+            // }
+          } 
+          else {
+            figures[j].style.display = "none";
+            // let figcaption = figures.querySelector("figcaption");
+            
+            // if (figcaption && photos[j].style.display === "none") {
+            //   figcaption.style.display = "none";
+            // } 
+            // else if (figcaption) {
+            //   figcaption.style.display = "block";
+            // }
+          }
+        }
+      });
+  
+      buttonContainer.appendChild(button);
+    }
+  }
 
 
   fetch("http://localhost:5678/api/categories")
   .then((res) => res.json())
-  .then((dataRecovery) => {
-    // regarder ce qu'on reçoit pour bien cibler l'objet
-    createEvenCategory(dataRecovery)
+  .then((dataFilter) => {
+    buildFilter(dataFilter)
   })
  
   // Gestion d'erreur IMPORTANT
@@ -59,11 +104,6 @@ fetch("http://localhost:5678/api/works")
     // OU mieux : créer une fonction qui affiche l'erreur dans une modal, un coin du site...
   });
 
-  function createEvenCategory(dataRecovery) {
-    console.log(dataRecovery);
-    const clickImg = document.querySelectorAll("img");
-   
-    clickImg.addEventLisner("click"), () => {
-      console.log("hello");
-    }
-  }
+  
+
+
